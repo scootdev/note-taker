5d666df525f1f27b814aa1259886223236b15d54
Scott Harris <scottsharris@yahoo.com>
2020-06-16 11:24:25 -0700

    used Date.now() to add unique id's to each note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,15 +2,32 @@ const express = require("express");
 const db = require("../db/db.json");
 const fs = require("fs");
 const router = express.Router();
+let database = db;
 
 router.get("/notes", (req, res) => {
     res.json(db);
 })
 
 router.post("/notes", (req, res) => {
-    db.push(req.body);
+    database.push(req.body);
     res.json(req.body);
-    fs.writeFile("./db/db.json", JSON.stringify(db), (err) => {
+    fs.writeFile("./db/db.json", JSON.stringify(database), (err) => {
+        if (err) {
+            return console.log(err);
+        }
+    })
+})
+
+router.delete("/notes/:id", (req, res) => {
+    const id = req.params.id;
+
+    for (let i = 0; i < database.length; i++) {
+        if (id === database[i].id) {
+           database.splice(i,1);
+           res.json(true);
+        }
+    }
+    fs.writeFile("./db/db.json", JSON.stringify(database), (err) => {
         if (err) {
             return console.log(err);
         }
